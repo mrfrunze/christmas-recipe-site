51d59e9700c7fd5b54e29e7e2981784782a04acf
Ilya <stoynov.oleg@gmail.com>
2025-11-06 19:41:01 +0100

    commit
    add product at home page

--- components/RecetionsGrid.tsx
+++ components/RecetionsGrid.tsx
@@ -11,7 +11,7 @@ export default function RecetionsGrid() {
   return (
     <section className="py-10">
       <div className="max-w-6xl mx-auto px-4">
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div className="grid grid-cols-1 lg:grid-cols-4 gap-6">
           {recipes.map((recipe) => (
             <RecipeCard key={recipe.id} recipe={recipe} />
           ))}
--- components/RecipeCard.tsx
+++ components/RecipeCard.tsx
@@ -1,5 +1,9 @@
 import Image from "next/image";
 import { Recipe } from "../lib/types";
+import { CiClock2 } from "react-icons/ci";
+import { PiCookingPotFill, PiCookingPotDuotone, PiCookingPot  } from "react-icons/pi";
+
+
 
 type RecipeCardProps = {
   recipe: Recipe;
@@ -12,9 +16,15 @@ export const RecipeCard: React.FC<RecipeCardProps> = ({ recipe }) => {
         <Image src={recipe.imageUrl} alt={recipe.title} fill className="object-cover" />
       </div>
 
-      <div className="p-4 flex items-center justify-between">
-        <h3 className="font-semibold text-lg">{recipe.title}</h3>
-        <span className="text-sm text-gray-600">{recipe.timeInMins} min</span>
+      <div className="py-2 px-1 flex flex-col justify-between">
+        <h3 className="text-[16px] text-center">{recipe.title}</h3>
+        <div className="flex items-center justify-center text-sm text-gray-600 h-8 gap-2 bg-[#f5faf4]">
+          <CiClock2/>
+          <span>Under</span>
+          <span>{recipe.timeInMins} min</span>
+          <PiCookingPot/>
+          <span>Medel</span>
+        </div>
       </div>
     </article>
   );
